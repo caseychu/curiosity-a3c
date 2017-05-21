23df010ab8e935e5fcfa9d0f4dcd5ab6d75a6191
Casey Chu <caseychu@stanford.edu>
2017-05-21 16:37:18 -0700

    Rough outline of worker

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import tensorflow as tf
 import numpy as np
+import gym
+from gym import wrappers
+from threading import Thread
+
+class Config:
+    beta = .1
+    lr = .1
 
 class PolicyNetwork():
-    def __init__(self, state_shape, num_actions, scope, reuse=False):
+    def __init__(self, config, scope):
+        self.config = config
+        
+        config.state_shape, num_actions
         self.beta = .1
         self.lr = .1
         self.num_actions = num_actions
@@ -15,12 +25,12 @@ class PolicyNetwork():
             biases_initializer: tf.zeros_initializer()
         }
         
-        with tf.variable_scope(scope, reuse=reuse):
-            self.state = tf.placeholder(tf.uint8, shape=(None,) + self.state_shape)
-            self.rnn_state = tf.placeholder(tf.uint8, shape=(None,) + self.rnn_hidden_size)
+        with tf.variable_scope(scope):
+            self.state = tf.placeholder(tf.uint8, shape=(None,) + self.config.state_shape)
+            self.rnn_state = tf.placeholder(tf.uint8, shape=(None,) + self.config.rnn_hidden_size) # hidden size?
             
-            self.policy, self.value = make_architecture(state, rnn_state)
-            self.gradients = make_gradients(policy, value, state, action, future_reward)
+            self.policy, self.value, self.new_rnn_state = make_architecture(state, rnn_state)
+            self.optimizer, self.gradients = make_gradients(policy, value, state, action, future_reward)
     
     def make_architecture(self, state, rnn_state):
         out = state
@@ -50,60 +60,90 @@ class PolicyNetwork():
             activation_fn=None,
             **init_params
         )
-        return policy, value
+        return policy, value, new_state
     
     def make_gradients(self, policy, value, state, action, future_reward):
         log_policy = tf.log(policy)
         entropy = -tf.einsum('ij,ij->i', policy, log_policy)
-        action_log_prob = tf.einsum('ij,ij->i', log_policy, tf.one_hot(action, self.num_actions))
+        action_log_prob = tf.einsum('ij,ij->i', log_policy, tf.one_hot(action, self.config.num_actions))
         advantage = future_reward - value
         
-        policy_loss = tf.reduce_mean(action_log_prob*advantage + self.beta*entropy)
+        policy_loss = tf.reduce_mean(action_log_prob*advantage + self.config.beta*entropy)
         value_loss = tf.reduce_mean(advantage**2)
         loss = policy_loss + value_loss
     
         optimizer = tf.train.AdamOptimizer(self.lr)
-        grads, variables = zip(*optimizer.compute_gradients(loss,
-            var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope)))
-        grads, _ = tf.clip_by_global_norm(grads, self.grad_clip)
-        return optimizer.apply_gradients(zip(grads, variables))
+        gradients, variables = zip(*optimizer.compute_gradients(loss, var_list=get_variables(self.scope)))
+        gradients, _ = tf.clip_by_global_norm(grads, self.grad_clip)
+        return optimizer, gradients
         
-    def copy_from(self, sess, scope):
-        src = sorted(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope), key=lambda v: v.name)
-        target = sorted(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope), key=lambda v: v.name)
+    def copy_from(self, sess, other_policy_network):
+        src = get_variables(other_policy_network.scope)
+        target = get_variables(self.scope)
         
         assignments = [tf.assign(t, s) for s, t in zip(src, target)]
-        return sess.run(tf.group(*assignments))
+        return sess.run(tf.group(*assignments))        
     
-    def get_policy(self, sess, state, rnn_state=np.zeros()):
-        return sess.run(self.policy, {
+    def get_policy(self, sess, state, rnn_state):
+        return sess.run([self.policy, self.value, self.new_rnn_state], {
             self.state: state,
             self.rnn_state: rnn_state
         })
     
-    def apply_gradients(self, sess, history):
-        return sess.run(self.apply, gr)
+    def apply_gradients(self, sess, grads):
+        return sess.run(self.optimizer.apply_gradients(zip(grads, get_variables(self.scope))))
 
-class PolicyExplorer():
-    pass
+def get_variables(scope):
+    return sorted(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope), key=lambda v: v.name)
+
+def worker(coord, sess, histories, policy_network, max_episode_len=20):
+    env = gym.make('CartPole-v0')
+    local_policy_network = PolicyNetwork(policy_network.config)
+    
+    while not coord.should_stop():
+        local_policy_network.copy_from(sess, policy_network)
+        
+        state = env.reset()
+        rnn_state = np.zeros()
+        
+        history = []
+        done = False
+        value = 0
+        for _ in xrange(max_episode_len):
+            policy, value, rnn_state = local_policy_network.get_policy(sess, state, rnn_state)
+            action = np.random.choice(num_actions, p=policy)
+            obs, reward, done, _ = env.step(action)
+            
+            history.append((state, action, reward, value, rnn_state))
+            if done:
+                break
+        
+        future_reward = 0 if done else value
+        gradients = []
+        for state, action, reward, value, rnn_state in reversed(history):
+            future_reward = reward + gamma * future_reward
+            gradients.append(local_policy_network.get_gradients(value, state, action, future_reward))
+        
+        total_gradients = [sum(step[i] for step in gradients) for i in range(len(gradients[0]))]
+        policy_network.apply_gradients(sess, total_gradients)
 
 def main():
+    env = gym.make('')
+    env.action_space.n
+    env.observation_space.n
+    env = wrappers.Monitor(env, 'results/experiments')
+    # preprocess frames?
     
-    # Initialize actor-critic network
+    policy_network = PolicyNetwork(scope='global')
+    sess = tf.Session()
     
     # Initialize workers
-    
-    # Apply async updates
+    coord = tf.train.Coordinator()
 
-    pass
+    threads = [Thread(target=worker, args=(coord, sess)) for i in xrange(10)]
+    for t in threads:
+        t.start()
+    
+    coord.join(threads)
 
 main()
-
-# Make Tensorflow behave like Numpy!
-def lift(construct_graph):
-    def construct(**placeholders):
-        out = construct_graph(**placeholders)
-        def run(sess, **kwargs):
-            return sess.run(out, feed_dict=dict(zip(placeholders, kwargs)))
-        return run    
-    return construct
