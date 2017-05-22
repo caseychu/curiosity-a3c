d3a588d749fb285cbb05d21dd7dad4cb094250a3
Casey Chu <caseychu@stanford.edu>
2017-05-21 23:27:58 -0700

    Gradient computation should be right

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class PolicyNetwork():
         self.num_actions = num_actions
         self.grad_clip = 10
         self.scope = scope
-        self.rnn_hidden_size = 100
+        self.config.rnn_hidden_size = 256
         
         init_params = {
             weights_initializer: tf.contrib.layers.xavier_initializer(),
@@ -26,14 +26,14 @@ class PolicyNetwork():
         }
         
         with tf.variable_scope(scope):
-            self.state = tf.placeholder(tf.uint8, shape=(None,) + self.config.state_shape)
+            self.states = tf.placeholder(tf.uint8, shape=(None,) + self.config.state_shape)
             self.rnn_state = tf.placeholder(tf.uint8, shape=(None,) + self.config.rnn_hidden_size) # hidden size?
             
-            self.policy, self.value, self.new_rnn_state = make_architecture(state, rnn_state)
+            self.policy, self.value, self.new_rnn_state = make_architecture(states, rnn_state)
             self.optimizer, self.gradients = make_gradients(policy, value, state, action, future_reward)
     
-    def make_architecture(self, state, rnn_state):
-        out = state
+    def make_architecture(self, states, initial_rnn_state):
+        out = states
         for i in range(4):
             out = tf.contrib.layers.conv2d(
                 inputs=out,
@@ -44,33 +44,51 @@ class PolicyNetwork():
                 activation_fn=tf.nn.elu,
                 **init_params)
 
-        lstm = tf.contrib.rnn.BasicLSTMCell(256)
         out = tf.contrib.layers.flatten(inputs=out)
-        out, new_state = lstm(rnn_state, out)
-
-        policy = tf.contrib.layers.fully_connected(
+        out, final_rnn_state = tf.nn.dynamic_rnn(
+            tf.contrib.rnn.BasicLSTMCell(self.config.rnn_hidden_size),
+            tf.expand_dims(states, 0),
+            initial_state=tf.expand_dims(initial_rnn_state, 0))
+        out = tf.squeeze(out, [0])
+        
+        policy_logits = tf.contrib.layers.fully_connected(
             inputs=out,
             num_outputs=num_actions,
-            activation_fn=tf.nn.softmax,
+            activation_fn=None,
             **init_params
         )
-        value = tf.contrib.layers.fully_connected(
+        values = tf.contrib.layers.fully_connected(
             inputs=out,
             num_outputs=1,
             activation_fn=None,
             **init_params
         )
-        return policy, value, new_state
-    
-    def make_gradients(self, policy, value, state, action, future_reward):
-        log_policy = tf.log(policy)
-        entropy = -tf.einsum('ij,ij->i', policy, log_policy)
-        action_log_prob = tf.einsum('ij,ij->i', log_policy, tf.one_hot(action, self.config.num_actions))
-        advantage = future_reward - value
         
-        policy_loss = tf.reduce_mean(action_log_prob*advantage + self.config.beta*entropy)
-        value_loss = tf.reduce_mean(advantage**2)
-        loss = policy_loss + value_loss
+        log_policies = tf.log_softmax(policy_logits)
+        policies = tf.softmax(policy_logits)
+        return policies, values, final_rnn_state
+    
+    def get_next_action(self, state, rnn_state):
+       
+    
+    # Make a loss from a rollout; the arguments should all be placeholders
+    def make_loss(self, states, actions, estimated_values, empirical_values):
+        
+        length = len(states)
+        discounting_matrix = np.fromfunction(lambda i,j: self.gamma**(j-i)*np.less_equal(i,j), shape=(length,length))
+        empirical_values = tf.matmul(discounting_matrix, rewards)
+        
+        policy_loss = tf.einsum('ij,ij,i->',
+            log_policies,
+            tf.one_hot(actions, self.config.num_actions),
+            empirical_values - estimated_values)
+        
+        entropy_loss = -(-tf.einsum('ij,ij->', policies, log_policies))
+        
+        value_loss = tf.nn.l2_loss(empirical_values - values)
+        
+        return policy_loss + entropy_loss + value_loss
+    
     
         optimizer = tf.train.AdamOptimizer(self.lr)
         gradients, variables = zip(*optimizer.compute_gradients(loss, var_list=get_variables(self.scope)))
@@ -96,7 +114,7 @@ class PolicyNetwork():
 def get_variables(scope):
     return sorted(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope), key=lambda v: v.name)
 
-def worker(coord, sess, histories, policy_network, max_episode_len=20):
+def worker(coord, sess, policy_network, max_episode_len=20):
     env = gym.make('CartPole-v0')
     local_policy_network = PolicyNetwork(policy_network.config)
     
@@ -112,19 +130,25 @@ def worker(coord, sess, histories, policy_network, max_episode_len=20):
         for _ in xrange(max_episode_len):
             policy, value, rnn_state = local_policy_network.get_policy(sess, state, rnn_state)
             action = np.random.choice(num_actions, p=policy)
-            obs, reward, done, _ = env.step(action)
+            new_state, reward, done, _ = env.step(action)
             
-            history.append((state, action, reward, value, rnn_state))
+            history.append((state, action, reward, value))
+            state = new_state
             if done:
                 break
         
-        future_reward = 0 if done else value
+        if done:
+            future_reward = 0
+        else:
+            _, value, _ = local_policy_network.get_policy(sess, state, rnn_state)
+            future_reward = value
+        
         gradients = []
         for state, action, reward, value, rnn_state in reversed(history):
             future_reward = reward + gamma * future_reward
-            gradients.append(local_policy_network.get_gradients(value, state, action, future_reward))
+            gradients.append(local_policy_network.get_gradients(value, state, action, future_reward, rnn_state))
         
-        total_gradients = [sum(step[i] for step in gradients) for i in range(len(gradients[0]))]
+        total_gradients = map(sum, zip(*gradients))
         policy_network.apply_gradients(sess, total_gradients)
 
 def main():
@@ -139,8 +163,7 @@ def main():
     
     # Initialize workers
     coord = tf.train.Coordinator()
-
-    threads = [Thread(target=worker, args=(coord, sess)) for i in xrange(10)]
+    threads = [Thread(target=worker, args=(coord, sess, policy_network)) for i in xrange(10)]
     for t in threads:
         t.start()
     
