790c0a5004e0aaac0561399aae77d258f6c2c9c0
Casey Chu <caseychu@stanford.edu>
2017-05-30 15:48:12 -0700

    Fixed race condition

--- main.py
+++ main.py
@@ -5,20 +5,26 @@ from gym import wrappers
 from threading import Thread
 import sys
 
+import time
+from test_env import EnvTest
+
 class Config:
     num_actions = 1
     state_shape = (1,)
     
     beta = .1
-    lr = .1
+    lr = 1e-4
     grad_clip = 10
     rnn_hidden_size = 256
     gamma = .99
 
 class PolicyNetwork():
-    def __init__(self, config, scope):
+    def __init__(self, config, scope, parent_network=None):
         self.config = config
         self.scope = scope
+
+        if parent_network is None:
+            parent_network = self
         
         init_params = {
             "weights_initializer": tf.contrib.layers.xavier_initializer(),
@@ -68,8 +74,13 @@ class PolicyNetwork():
             self.log_policies = log_policies
             self.policies = policies
             self.values = values
-            self.all_variables = sorted(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope), key=lambda v: v.name)
-            print [v.name for v in self.all_variables]
+            
+            # Synchronizing variables.
+            all_variables = sorted(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope), key=lambda v: v.name)
+            self.all_variables = all_variables
+            
+            sync_op = tf.group(*[tf.assign(t, s) for s, t in zip(parent_network.all_variables, all_variables)])
+            self.sync_op = sync_op
 
             # Gradients.
             self.actions = actions = tf.placeholder(tf.uint8, shape=(None,))
@@ -84,26 +95,24 @@ class PolicyNetwork():
             value_loss = tf.nn.l2_loss(empirical_values - values)
             loss = policy_loss + entropy_loss + value_loss
             
-            grads = tf.gradients(loss, self.all_variables)
+            grads = tf.gradients(loss, all_variables)
             grads, grad_norm = tf.clip_by_global_norm(grads, self.config.grad_clip)
+            optimizer = tf.train.AdamOptimizer(self.config.lr)
+            train_op = optimizer.apply_gradients(zip(grads, parent_network.all_variables))
             
             self.grads = grads
             self.grad_norm = grad_norm
             self.loss = loss
-            self.optimizer = tf.train.AdamOptimizer(self.config.lr)
-            _ = self.optimizer.minimize(loss) # We need to create this op before tf.global_variables_initializer
+            self.optimizer = optimizer
+            self.train_op = train_op
             
             # Summaries
             tf.summary.scalar('loss', loss)
             tf.summary.scalar('gradient norm', grad_norm)
             self.summaries = tf.summary.merge_all()
         
-    def copy_from(self, sess, other_policy_network):
-        src = other_policy_network.all_variables
-        target = self.all_variables
-        
-        assignments = [tf.assign(t, s) for s, t in zip(src, target)]
-        return sess.run(tf.group(*assignments))        
+    def synchronize(self, sess):
+        return sess.run(self.sync_op)
     
     def get_next_policy(self, sess, state, rnn_state):
         if rnn_state is None:
@@ -119,27 +128,21 @@ class PolicyNetwork():
             
         return policies[0], values[0], next_rnn_state
     
-    def get_gradients_from_rollout(self, sess, states, actions, estimated_values, empirical_values):
-        g, l = sess.run([self.grads, self.grad_norm], feed_dict={
+    def apply_gradients_from_rollout(self, sess, states, actions, estimated_values, empirical_values):
+        g, gn, l, _ = sess.run([self.grads, self.grad_norm, self.loss, self.train_op], feed_dict={
             self.states: states,
             self.actions: actions,
             self.estimated_values: estimated_values,
             self.empirical_values: empirical_values
         })
-        print 'loss: ', l
+        #print self.scope, 'grad norm: ', gn, '; loss: ', l
         sys.stdout.flush()
         return g
-    
-    def apply_gradients(self, sess, grads):
-        return sess.run(self.optimizer.apply_gradients(zip(grads, self.all_variables)))
 
-def worker(coord, worker_id, sess, graph, policy_network, max_episode_length=20):
-    with graph.as_default():
-        env = gym.make('Breakout-v0')
-        local_policy_network = PolicyNetwork(policy_network.config, worker_id)
-    
+def worker(coord, sess, policy_network, env, max_episode_length=20):
+    with coord.stop_on_exception():
         while not coord.should_stop():
-            local_policy_network.copy_from(sess, policy_network)
+            policy_network.synchronize(sess)
             rnn_state = None
             state = env.reset()
 
@@ -147,8 +150,8 @@ def worker(coord, worker_id, sess, graph, policy_network, max_episode_length=20)
             done = False
             value = 0
             for _ in xrange(max_episode_length):
-                policy, value, rnn_state = local_policy_network.get_next_policy(sess, state, rnn_state)
-                action = np.random.choice(local_policy_network.config.num_actions, p=policy)
+                policy, value, rnn_state = policy_network.get_next_policy(sess, state, rnn_state)
+                action = np.random.choice(policy_network.config.num_actions, p=policy)
                 new_state, reward, done, _ = env.step(action)
 
                 history.append((state, action, value, reward))
@@ -157,19 +160,21 @@ def worker(coord, worker_id, sess, graph, policy_network, max_episode_length=20)
                     break
 
             states, actions, estimated_values, rewards = map(np.array, zip(*history))
-            print 'true reward: ', np.sum(rewards)
-            
+            print policy_network.scope, 'true reward: ', np.sum(rewards)
+
             future_rewards = [0 if done else value]
             for reward in reversed(rewards.tolist()[:-1]):
                 future_rewards.append(reward + policy_network.config.gamma * future_rewards[-1])
             empirical_values = np.array(list(reversed(future_rewards)))
-            
-            gradients = local_policy_network.get_gradients_from_rollout(sess, states, actions, estimated_values, empirical_values)
-            policy_network.apply_gradients(sess, gradients)
+
+            policy_network.apply_gradients_from_rollout(sess, states, actions, estimated_values, empirical_values)
+
 
 def main():
-    env = gym.make('Breakout-v0')
-    
+    make_env = lambda: EnvTest((5, 5, 1))
+    #gym.make('Pong-v0')
+        
+    env = make_env()
     config = Config()
     config.num_actions = env.action_space.n
     config.state_shape = env.observation_space.shape
@@ -180,16 +185,21 @@ def main():
     policy_network = PolicyNetwork(config, 'global')
     
     sess = tf.Session()
+    coord = tf.train.Coordinator()
+    threads = []
+    for i in xrange(2):
+        local_env = make_env()
+        local_policy_network = PolicyNetwork(config, 'worker' + str(i), parent_network=policy_network)
+        threads.append(Thread(target=worker, args=(coord, sess, local_policy_network, local_env)))
+    
     sess.run(tf.global_variables_initializer())
     
-    # Initialize workers
-    graph = tf.get_default_graph()
-    coord = tf.train.Coordinator()
-    
-    threads = [Thread(target=worker, args=(coord, 'worker' + str(i), sess, graph, policy_network)) for i in xrange(1)]
-    with coord.stop_on_exception():
+    try:
         for t in threads:
             t.start()
-        coord.join(threads)
+        coord.join(threads, stop_grace_period_secs=5)
+    except KeyboardInterrupt:
+        coord.request_stop()
+        raise
 
 main()
